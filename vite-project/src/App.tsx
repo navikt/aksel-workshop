d4888028b446a1c34bf67145a111fc92f70c3c1e
Ken <ken.aleksander@gmail.com>
2023-05-30 12:59:00 +0200

    :art: Boilerplate

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <h1>hello</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam eius illo dolore
+        dicta voluptas. Laboriosam recusandae voluptates ipsum totam, dolorum aliquid
+        officiis ex, sequi maxime laudantium, suscipit quod exercitationem molestiae!
+      </p>
+    </main>
+  );
+}
+
+export default App;
