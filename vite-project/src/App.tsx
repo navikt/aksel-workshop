cc1593350b22fdfcb7e3607d59170aa548e9f003
Ken <ken.aleksander@gmail.com>
2023-05-30 13:11:33 +0200

    :memo: Intro-tekster

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <main>
-      <h1>hello</h1>
+      <h1>hello world!</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam eius illo dolore
         dicta voluptas. Laboriosam recusandae voluptates ipsum totam, dolorum aliquid
