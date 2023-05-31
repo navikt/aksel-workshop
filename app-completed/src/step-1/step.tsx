bbb98c149d62318ec2a1d01fb2199d6f11617f6c
Ken <ken.aleksander@gmail.com>
2023-05-31 19:32:54 +0200

    :art: Step-1 completed

--- app-completed/src/step-1/step.tsx
+++ app-completed/src/step-1/step.tsx
@@ -1,16 +1,41 @@
-import { Link } from "react-router-dom";
 import "./step.css";
+import { Button, Heading, Radio, RadioGroup, ReadMore } from "@navikt/ds-react";
 
 function Step() {
   return (
     <main>
-      <h1>Steg 1</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam eius illo dolore
-        dicta voluptas. Laboriosam recusandae voluptates ipsum totam, dolorum aliquid
-        officiis ex, sequi maxime laudantium, suscipit quod exercitationem molestiae!
-      </p>
-      <Link to="/steg-2">Steg 2</Link>
+      <Heading size="large" level="1" spacing>
+        Arbeid
+      </Heading>
+
+      <form onSubmit={(e) => e.preventDefault()} className="form-layout">
+        <div>
+          <RadioGroup legend="Får du AAP nå?">
+            <Radio value="ja">Ja</Radio>
+            <Radio value="nei">Nei</Radio>
+          </RadioGroup>
+          <ReadMore header="Hvorfor spør vi om du får AAP nå?">
+            Det er ulike regler for hvor mye du kan jobbe når:
+            <ul>
+              <li>du søker AAP</li>
+              <li>etter du har fått innvilget AAP</li>
+            </ul>
+          </ReadMore>
+        </div>
+        <div>
+          <RadioGroup legend="Jobber du nå?">
+            <Radio value="ja">Ja</Radio>
+            <Radio value="nei">Nei</Radio>
+          </RadioGroup>
+          <ReadMore header="Hvorfor spør vi om du har jobb?">
+            Det er ulike regler for hvor mye du kan jobbe når du søker AAP og etter at du
+            mottar AAP.
+          </ReadMore>
+        </div>
+        <Button as="a" href="/steg-2">
+          Gå videre
+        </Button>
+      </form>
     </main>
   );
 }
